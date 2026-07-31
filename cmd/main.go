91aa10ed04706378dce76989aa0920f178f39477
sebastian <sebascova18@gmail.com>
2026-07-31 19:01:07 +0000

    refactor(cmd): se implementa timeout y cambios de eventos de cargas

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/mxschmitt/playwright-go"
+	"log"
 )
 
 func main() {
+	err := playwright.Install(&playwright.RunOptions{
+		Browsers: []string{"chromium"},
+	})
+	if err != nil {
+		log.Fatalf("no se pudo instalar el driver de playwright: %v", err)
+	}
 	pw, err := playwright.Run()
 	if err != nil {
 		panic(err)
@@ -32,13 +39,17 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	_, err = page.Goto("https://aternos.org")
+      
+	// cambiamos el evento de confirmacion de carga(load) , por el evento domloaded
+	_, err = page.Goto("https://aternos.org", playwright.PageGotoOptions{
+	   WaitUntil: playwright.WaitUntilStateDomcontentloaded,
+	   Timeout: playwright.Float(60000),
+	})
 	if err != nil {
 		panic(err)
 	}
 
-	page.WaitForTimeout(10000)
+	page.WaitForTimeout(60000)
 	_, err = context.StorageState(playwright.BrowserContextStorageStateOptions{
 		Path: playwright.String("storage/state.json"),
 	})
