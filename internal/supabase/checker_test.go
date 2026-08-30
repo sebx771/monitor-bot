fed49c84565a667783758c668bb3ec81f86ffbd9
Sebastian <sebascova18@gmail.com>
2026-08-30 13:19:16 -0500

    feat(supabas/test): agrega test para el cheker del servicio supabase

---
+++ internal/supabase/checker_test.go
@@ -0,0 +1,135 @@
+package supabase
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"sync/atomic"
+	"testing"
+
+	"github.com/sebx771/monitor-bot/internal/logger"
+)
+
+func newTestClient(token, baseURL string) *Client {
+
+	return &Client{
+		token:      token,
+		baseUrl:    baseURL,
+		httpClient: &http.Client{},
+	}
+}
+
+func TestCheck_Checker_ListProjectError(t *testing.T) {
+
+	server := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		if req.Method == http.MethodGet {
+
+			res.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+	}))
+	defer server.Close()
+	checker := InitChecker(server.URL)
+	err := checker.Check()
+
+	if err == nil {
+		t.Fatalf("want error , got nil")
+	}
+
+}
+
+func TestCheck_Checker_ActiveProjects(t *testing.T) {
+	var pots atomic.Int32
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			json.NewEncoder(w).Encode([]Project{
+				{ID: "1", Ref: "123", Name: "mysql", Status: "ACTIVE"},
+			})
+			return
+		}
+
+		if r.Method == http.MethodPost {
+			pots.Add(1)
+			w.WriteHeader(http.StatusAccepted)
+		}
+	}))
+	defer server.Close()
+	checker := InitChecker(server.URL)
+	checker.Check()
+
+	if pots.Load() != 0 {
+		t.Fatalf("se esperaba 0 POST, se obtuvieron %d", pots.Load())
+	}
+
+}
+
+func TestCheck_Checker_InactiveProjects(t *testing.T) {
+	var pots atomic.Int32
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			json.NewEncoder(w).Encode([]Project{
+				{ID: "2", Ref: "1234", Name: "mysql", Status: "INACTIVE"},
+			})
+			return
+		}
+
+		if r.Method == http.MethodPost {
+			pots.Add(1)
+			w.WriteHeader(http.StatusAccepted)
+		}
+	}))
+	defer server.Close()
+	checker := InitChecker(server.URL)
+	checker.Check()
+
+	if pots.Load() != 1 {
+		t.Fatalf("se esperaba 1 PUT, se obtuvieron %d", pots.Load())
+	}
+
+}
+
+func TestCheck_Checker_RestoreProjectError(t *testing.T) {
+	var posts atomic.Int32
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			json.NewEncoder(w).Encode([]Project{
+				{
+					ID:     "2",
+					Ref:    "1234",
+					Name:   "mysql",
+					Status: "INACTIVE",
+				},
+			})
+			return
+		}
+
+		if r.Method == http.MethodPost {
+			posts.Add(1)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}))
+
+	defer server.Close()
+
+	checker := InitChecker(server.URL)
+
+	err := checker.Check()
+
+	if err != nil {
+		t.Fatalf("se esperaba nil, se obtuvo %v", err)
+	}
+
+	if posts.Load() != 1 {
+		t.Fatalf("se esperaba 1 POST, se obtuvieron %d", posts.Load())
+	}
+}
+
+func InitChecker(URL string) Checker {
+	logg := logger.NewLogger("TEST")
+	client := newTestClient("tk", URL)
+	checker := NewChecker(client, logg)
+	return *checker
+}
